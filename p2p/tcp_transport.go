35cdc9a074e161bdd704c9c47f7d6ce813072098
Cheemx <chinmaymahajan999@gmail.com>
2025-09-23 14:55:25 +0000

    protobuf transfer working!

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,20 +1,92 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
 
+// TCPPeer represents the remote node over a TCP established connection.
+type TCPPeer struct {
+	// conn is the underlying connection of the peer.
+	conn net.Conn
+
+	// If we dial and retrieve a conn => true
+	// If we accept and retrieve a conn => false
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransportOpts struct {
+	ListenAddr    string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+}
+
 type TCPTransport struct {
-	listenAddress string
-	listener      net.Listener
+	TCPTransportOpts
+	listener net.Listener
 
 	mu    sync.RWMutex
 	peers map[net.Addr]Peer
 }
 
-func NewTCPTransport(listenAddr string) *TCPTransport {
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
 	return &TCPTransport{
-		listenAddress: listenAddr,
+		TCPTransportOpts: opts,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %v\n", err)
+		}
+		fmt.Printf("New Incoming Connection: %+v", conn)
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+
+	if err := t.HandshakeFunc(peer); err != nil {
+		conn.Close()
+		fmt.Printf("TCP handshake error: %v\n", err)
+		return
+	}
+
+	// Read Loop
+	msg := &Message{}
+	for {
+		// n, err := conn.Read(buf)
+		// if err != nil {
+		// 	fmt.Printf("TCP error: %v\n", err)
+		// }
+		if err := t.Decoder.Decode(conn, msg); err != nil {
+			fmt.Printf("TCP error: %v\n", err)
+			continue
+		}
+		msg.From = conn.RemoteAddr()
+		fmt.Printf("message: %+v\n", msg)
 	}
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -1,8 +1,6 @@
 package p2p
 
 import (
-	"fmt"
-	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,31 +11,6 @@ func TestTCPTransport(t *testing.T) {
 	tr := NewTCPTransport(listenAddr)
 
 	assert.Equal(t, tr.listenAddress, listenAddr)
-}
 
-func (t *TCPTransport) ListenAndAccept() error {
-	var err error
-	t.listener, err = net.Listen("tcp", t.listenAddress)
-	if err != nil {
-		return err
-	}
-
-	go t.startAcceptLoop()
-
-	return nil
-}
-
-func (t *TCPTransport) startAcceptLoop() {
-	for {
-		conn, err := t.listener.Accept()
-		if err != nil {
-			fmt.Printf("TCP accept error: %v\n", err)
-		}
-
-		go t.handleConn(conn)
-	}
-}
-
-func (t *TCPTransport) handleConn(conn net.Conn) {
-	fmt.Printf("New Incoming Connection: %+v\n", conn)
+	assert.Nil(t, tr.ListenAndAccept())
 }
